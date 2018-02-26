2b7160482b2a852dbb3a864a069cc80e33eb0585
Semen Shaplygin <semyuon@gmail.com>
2018-02-26 17:47:59 +0300

    setup from file

--- render.js
+++ render.js
@@ -79,4 +79,7 @@ function setupDirectory(){
 function readDirectory(){
    var res = fs.readFileSync('path.txt', 'utf-8');
    pathFile = res;
+   let setupDir = document.getElementById('setupDir');
+   setupDir.files[0].path = pathFile.slice();
+
 }
